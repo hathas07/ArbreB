d8a9ac61be1e6d8a1b6fc37a05548bec032f53a5
hathas07 <35289971+hathas07@users.noreply.github.com>
2020-01-14 09:54:11 +0100

    Ajout de la fonction Rechercher

--- src/arbre.java
+++ src/arbre.java
@@ -1,4 +1,27 @@
 public class arbre {
 
+    protected int M;
+    protected noeud Racine;
+
+    private noeud RechercherArbre(int Valeur, noeud Arbre) {
+        int indice = 0;
+
+        if (Arbre != null) {
+            while (Valeur > Arbre.tabCles[indice]) {
+                indice++;
+            }
+            if (Valeur == Arbre.tabCles[indice]) {
+                return Arbre;
+            } else RechercherArbre(Valeur, Arbre.tabPointeur[indice]);
+        }
+
+        return null;
+    }
+
+
+    public static void main(String[] args){
+
+        System.out.println("dd");
+    }
 
 }
