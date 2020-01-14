252944803e4312a7c7baf8b86eacf6453b76b26d
hathas07 <35289971+hathas07@users.noreply.github.com>
2020-01-14 10:44:59 +0100

    rien à commit

--- src/arbre.java
+++ src/arbre.java
@@ -1,7 +1,7 @@
 public class arbre {
 
-    protected int M;
-    protected noeud Racine;
+    protected static int M;
+    protected static noeud Racine;
 
     private noeud RechercherArbre(int Valeur, noeud Arbre) {
         int indice = 0;
@@ -18,10 +18,17 @@ public class arbre {
         return null;
     }
 
-
-    public static void main(String[] args){
-
-        System.out.println("dd");
+    private static void AfficherArbre(noeud Arbre) {
+        int indice = 0;
+    while (Arbre.tabCles[indice] < 2*M){
+        System.out.print(Arbre.tabCles[indice]);
+    }
     }
 
+    public static void main(String[] args){
+        M = 2;
+        noeud Arbre = new noeud(M);
+        Arbre.tabCles = new int[]{3, 8, 12};
+        AfficherArbre(Racine);
+    }
 }
