de1194f093a6888d0841e7738e314768d20e5ae6
hathas07 <35289971+hathas07@users.noreply.github.com>
2020-01-15 10:06:23 +0100

    On commence l'insertion

--- src/arbre.java
+++ src/arbre.java
@@ -1,16 +1,18 @@
+import java.util.*;
+
 public class arbre {
 
     protected static int M;
     protected static noeud Racine;
 
-    private noeud RechercherArbre(int Valeur, noeud Arbre) {
+    private noeud RechercherArbre(Object Valeur, noeud Arbre) {
         int indice = 0;
-
+        Comparator c = null;
         if (Arbre != null) {
-            while (Valeur > Arbre.tabCles[indice]) {
+            while (c.compare(Valeur,Arbre.tabCles[indice]) > 0) {
                 indice++;
             }
-            if (Valeur == Arbre.tabCles[indice]) {
+            if (c.compare(Valeur,Arbre.tabCles[indice]) == 0) {
                 return Arbre;
             } else RechercherArbre(Valeur, Arbre.tabPointeur[indice]);
         }
@@ -18,17 +20,47 @@ public class arbre {
         return null;
     }
 
+    private noeud InsererDsFeuille(Object Cles, noeud Feuille){
+        if(Feuille.feuille){
+            Object Clestemp = 0;
+            Comparator c = null;
+            for(int i = 0;  i < Feuille.tabCles.length; i++){
+                if(c.compare(Cles,Feuille.tabCles[i]) <= 0){
+                    Clestemp = Feuille.tabCles[i];
+                    Feuille.tabCles[i] = Cles;
+                    Cles = Clestemp;
+                }
+            }
+            return Feuille;
+        }
+        return null;
+    }
+
+    private noeud Inserer(Object Cles, noeud Arbre){
+        if(Arbre.feuille){
+            Arbre = InsererDsFeuille(Cles, Arbre);
+            if(Arbre.getNbCles() == 2*M){
+
+            }
+        }
+        return null;
+    }
     private static void AfficherArbre(noeud Arbre) {
         int indice = 0;
-    while (Arbre.tabCles[indice] < 2*M){
+    while (indice < Arbre.tabCles.length){
         System.out.print(Arbre.tabCles[indice]);
+        indice++;
     }
     }
 
     public static void main(String[] args){
         M = 2;
-        noeud Arbre = new noeud(M);
-        Arbre.tabCles = new int[]{3, 8, 12};
+        noeud Racine = new noeud(M);
+        Racine.tabCles[0] = 3;
+        Racine.tabCles[1] = 3;
+        Racine.tabCles[2] = 3;
+        Racine.tabCles[3] = 3;
         AfficherArbre(Racine);
+        System.out.print(Racine.tabCles.length);
     }
 }
--- src/noeud.java
+++ src/noeud.java
@@ -1,30 +1,32 @@
+import java.util.*;
 public class noeud {
 
     int NbCles;
-    int [] tabCles;
+    Object [] tabCles;
     noeud [] tabPointeur;
     boolean feuille;
+    noeud pere;
 
     public noeud(int M){
-        int NbCles = 0;
-        int [] tabCles = new int [2*M];
-        noeud [] tabPointeur = new noeud[2*M];
-        boolean feuille = true;
+        NbCles = 0;
+        tabCles = new Object [2*M];
+        tabPointeur = new noeud[2*M];
+        feuille = true;
+        pere = null;
     }
 
-    public int getNbCles(){
-        return NbCles;
-    }
+    public int getNbCles(){ return NbCles; }
 
     public boolean getfeuille(){
         return feuille;
     }
 
-    public int [] gettabCles(){
+    public Object [] gettabCles(){
         return tabCles;
     }
 
     public noeud [] gettabPointeur(){
         return tabPointeur;
     }
+    
 }
